ade90553057a983ea26de8fcaeecf928249f3e34
Vijay Kedar <54878107+kedarvijay@users.noreply.github.com>
2022-01-26 22:04:18 +0530

    Create MyCalculator.java

---
+++ MyCalculator.java
@@ -0,0 +1,9 @@
+package com.intfc;
+
+public interface MyCalculator {
+int sum(int ...arg);           
+int square(int n);
+
+//we created two abstract methods sum and square and but it should have return type. 
+
+}
