79f65c36322924702e6462151d22a30b174aacb1
Vijay Kedar <54878107+kedarvijay@users.noreply.github.com>
2022-01-26 21:53:20 +0530

    Create MyMessage.java

---
+++ MyMessage.java
@@ -0,0 +1,6 @@
+package com.intfc;
+
+public interface MyMessage {
+	String sendMessage(String name); //this is by default public and abstract method and we can't write body in abstract methods.
+
+}
